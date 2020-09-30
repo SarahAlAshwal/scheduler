3a2fc6583e4cd59aad37678e04d5481b5b9e7ac6
SarahAlAshwal <sa.alashwal@gmail.com>
2020-09-30 19:54:07 -0400

    add Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,25 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classnames from 'classnames'
+
 export default function Button(props) {
-   return <></>;
+
+   // let buttonClass = 'button';
+   // if(props.confirm) {
+   //    buttonClass+= ' button--confirm'
+   // }
+
+   // if(props.danger) {
+   //    buttonClass += ' button--danger'
+   // }
+
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+
+  // const buttonClass = classNames('button', {'button--confirm': props.confirm}, {'button--danger': props.danger} );
+   
+   return <button onClick = { props.onClick} className={buttonClass} disabled= {props.disabled}>{props.children}</button>;
 }
