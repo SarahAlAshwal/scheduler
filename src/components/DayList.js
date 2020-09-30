431f7204d3e3f7117f047c706ab44122c224ab1f
SarahAlAshwal <sa.alashwal@gmail.com>
2020-09-30 19:59:01 -0400

    add DayList and DayListItem components

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+
+export default function DayList(props) {
+
+  //console.log(props.days);
+
+  const DayListArray = props.days.map((day) =>
+   <DayListItem  key= {day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />
+   
+  )
+
+  return <ul>
+    {DayListArray}
+  </ul>;
+
+}
+
+
+
+
+
---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "components/DayListItem.scss";
+import classnames from 'classnames'
+
+
+export default function DayListItem(props) {
+
+  const dayClass = classnames({'day-list__item':!(props.selected | (props.spots === 0))},{'day-list__item--selected': props.selected,'day-list__item--full': (props.spots === 0)} );
+  
+  function formatSpots (spots) {
+    let spotsText = `${spots} spots remaining`;
+    if (spots === 1) 
+      return spotsText = `${spots} spot remaining`;
+    else if ( spots === 0)
+       return spotsText = `no spots remaining`;
+    else
+      return spotsText;
+  }
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className='text--regular'>{props.name}</h2>
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
