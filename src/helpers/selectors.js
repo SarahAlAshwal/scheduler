b2d72334c692ad53b3e5ea1a11dce051e7794174
SarahAlAshwal <sa.alashwal@gmail.com>
2020-10-01 15:42:26 -0400

    create getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,3 +15,19 @@ export function getAppointmentsForDay(state, day) {
 return appointmentesArray;
 }
 
+export function getInterview(state, interview) {
+  const foundInterview = {};
+  if (interview) {
+    for (const interviewer in state.interviewers) {
+      if ( state.interviewers[interviewer].id === interview.interviewer) {
+        foundInterview['student'] = interview.student;
+        foundInterview['interviewer'] = state.interviewers[interviewer];
+      }
+    }
+  } else {
+    return null;
+  }
+
+  return foundInterview;
+}
+
